7fa2ce7c66f8875ba6faa8de66a6d7211df1cac6
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-07-27 12:23:07 +0530

    Create sumofk.java

---
+++ sumofk.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int K=s.nextInt();
+		int N=5;
+		int sum=0;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=K;j++)
+			{
+				sum=j+1;
+			}
+		}
+		System.out.println(sum);
+	}
+}
