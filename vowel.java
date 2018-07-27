3b7183f5682c7ffc5a8bc2740cbe38188144afe8
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-07-27 10:20:10 +0530

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -10,11 +10,11 @@ class Guvi
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		ch=(char)bf.read();
 		if(ch =='a'||ch =='e'||ch =='i'|| ch=='o'||ch =='u')
-			System.out.println("vowel");
+			System.out.println("Vowel");
 		else if(ch=='b'||ch=='c'||ch=='d'||ch=='f'||ch=='g'||ch=='h'||ch=='j'||ch=='k'||ch=='l'||ch=='m'||ch=='n'||ch=='p'||ch=='q'||ch=='r'||ch=='s'||ch=='t'||ch=='v'||ch=='w'||ch=='x'||ch=='y'||ch=='z')			
 			System.out.println("Consonant");
 		else
-			System.out.println("Invalid");
+			System.out.println("invalid");
 		
 			
 	}
