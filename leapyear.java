766daf873b03bbcbcd05fe0eb8a525490556d36e
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-07-27 10:50:47 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		if(a%4==0)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
