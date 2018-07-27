0e675181654e1ae44f11d26e67997b131ef16e08
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-07-27 10:59:51 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int i;
+		for(i=0;i<N;i++)
+		System.out.println("Hello");
+	}
+}
