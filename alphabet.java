1f52fd50ac643b331f029e770c419ad2fc5ca808
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-07-27 10:37:48 +0530

    Update alphabet.java

--- alphabet.java
+++ alphabet.java
@@ -5,6 +5,12 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		
+		char ch;
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		ch=(char)bf.read();
+		 if(ch=='a'||ch=='b'||ch=='c'||ch=='d'||ch=='e'||ch=='f'||ch=='g'||ch=='h'||ch=='i'||ch=='j'||ch=='k'||ch=='l'||ch=='m'||ch=='n'||ch=='o'||ch=='p'||ch=='q'||ch=='r'||ch=='s'||ch=='t'||ch=='u'||ch=='v'||ch=='w'||ch=='x'||ch=='y'||ch=='z')			
+			System.out.println("Alphabet");
+		else
+			System.out.println("No");
 	}
 }
