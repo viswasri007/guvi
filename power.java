44088f453de6d2e263b3cc8660348b0800d76d3c
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-07-27 12:38:53 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		System.out.println(Math.pow(n,m));
+	}
+}
