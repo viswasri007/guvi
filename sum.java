68d6a0875e0f8d6831bda88a2fc6a279bc82e2cd
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-07-27 11:06:09 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		n=(n*(n+1))/2;
+		System.out.println(n);
+	}
+}
