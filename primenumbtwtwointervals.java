88786ac0bf83ec85d780cd31cc09fb4cb359f2b6
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-08-01 13:53:39 +0530

    Create primenumbtwtwointervals.java

---
+++ primenumbtwtwointervals.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		for(int i=n;i<=m;i++)
+		{
+			int cnt=0;
+			for(int j=1;j<=i;j++)
+			{
+				if(i%j==0)
+				cnt++;
+			}
+			if(cnt==2)
+			System.out.println(i);
+		}
+	}
+}
