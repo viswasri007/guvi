63da7bb27a2007647e8bebca1e5ced12f327055c
viswasri007 <41729456+viswasri007@users.noreply.github.com>
2018-08-01 14:46:24 +0530

    Create firstmultiples.java

---
+++ firstmultiples.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			int res;
+			res=n*i;
+			System.out.println(res);
+		}
+		
+	}
+	
+}
